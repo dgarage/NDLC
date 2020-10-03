f87ac151bd85cc5aa7c6ea7317f6a92169098375
joe.miyamoto <joe.miyamoto@bitcoinbank.co.jp>
2020-10-03 20:50:58 +0900

    update GUI

--- NDLC.GUI/Controls.fs
+++ NDLC.GUI/Controls.fs
@@ -1,21 +1,29 @@
 namespace NDLC.GUI
+open System
 
 open Avalonia.Controls
 open Avalonia.FuncUI.DSL
 open Avalonia.Input
+open Avalonia.Interactivity
 
 module TextBox =
     let onTextInput handler =
+        let handler' (args: RoutedEventArgs) =
+            let tx = (args.Source :?> TextBox).Text
+            if String.IsNullOrEmpty tx then () else
+            handler tx
         [
-            TextBox.onKeyDown(fun args -> handler (args.Source :?> TextBox).Text)
-            TextBox.onKeyUp(fun args -> handler (args.Source :?> TextBox).Text)
+            TextBox.onKeyDown(handler')
+            TextBox.onKeyUp(handler')
         ]
         
     let onTextInputFinished handler =
         let handler' =
            fun (args: KeyEventArgs) ->
                if args.Key = Key.Enter || args.Key = Key.Tab then
-                   handler (args.Source :?> TextBox).Text
+                   let tx = (args.Source :?> TextBox).Text
+                   if String.IsNullOrEmpty tx then () else
+                   handler tx
         [
             TextBox.onKeyDown(handler')
             TextBox.onKeyUp(handler')
--- NDLC.GUI/DLC/DLCOffer.fs
+++ NDLC.GUI/DLC/DLCOffer.fs
@@ -15,44 +15,150 @@ open ResultUtils
 open NDLC.Infrastructure
 open NDLC.Messages
 
+open NBitcoin
+open System.Threading.Tasks
+open NDLC
 open NDLC.GUI
 open NDLC.GUI.GlobalMsgs
 open NDLC.GUI.Utils
 
-type OfferVM = {
-    ContractInfo: ContractInfo seq
+[<AutoOpen>]
+module private Helpers =
+    let tryParseContractInfo (str: string) =
+        match DiscretePayoffs.TryParse(str) with
+        | false, _ -> Error("Invalid payoffs!")
+        | true, c ->
+            if (c.Count < 2) then Error("You must specify at least 2 contract info!") else
+            Ok(c)
+            
+    let validateContractInfo(str: string) =
+        match tryParseContractInfo(str) with
+        | Ok _ -> None
+        | Error e -> Some e
+        
+    let tryParseLockTime (str: string) =
+        match UInt32.TryParse str with
+        | true, r -> Ok(LockTime(r))
+        | _ -> Error("Invalid input for locktime")
+        
+    let validateLockime(str: string) =
+        match tryParseLockTime(str) with
+        | Ok _ -> None
+        | Error e -> Some (e)
+        
+    let tryParseEventFullname (str: string) =
+        match EventFullName.TryParse str with
+        | true, r -> Ok(r)
+        | _ -> Error("Invalid input for EventFullName")
+        
+    let validateEventFullName(str: string) =
+        match tryParseEventFullname(str) with
+        | Ok _ -> None
+        | Error e -> Some (e)
+        
+    let tryParseFeeRate (str: string) =
+        match Int64.TryParse str with
+        | true, r -> Ok(FeeRate(Money (r)))
+        | _ -> Error("Invalid input for FeeRate")
+        
+    let validateFeeRate(str: string) =
+        match tryParseFeeRate(str) with
+        | Ok _ -> None
+        | Error e -> Some (e)
+        
+
+type ErrorState = private {
+    ContractInfoErr: string option
+    /// The locktime of the CET transaction
+    LockTimeErr: string option
+    RefundLockTimeErr: string option
+    EventFullNameErr: string option
+    LocalNameErr: string option
+    FeeRateErr: string option
+}
+
+type OfferDomainModel = {
+    ContractInfo: DiscretePayoffs
     /// The locktime of the CET transaction
     LockTime: LockTime
     RefundLockTime: LockTime
     EventFullName: EventFullName
     LocalName: string
+    FeeRate: FeeRate
+}
+
+type OfferVM = {
+    ContractInfo: string
+    /// The locktime of the CET transaction
+    LockTime: string
+    RefundLockTime: string
+    EventFullName: string
+    LocalName: string
+    FeeRate: string
 }
     with
     static member Empty = {
-        ContractInfo = Seq.empty
-        LockTime = LockTime.Zero
-        RefundLockTime = LockTime(4999999)
-        EventFullName = EventFullName.Empty
+        ContractInfo = ""
+        LockTime = ""
+        RefundLockTime = ""
+        EventFullName = ""
         LocalName = ""
+        FeeRate = ""
     }
     static member FromMetadata(m: NewOfferMetadata) = {
-        ContractInfo = Seq.empty
-        LockTime =  LockTime.Zero
-        RefundLockTime = LockTime(4999999)
-        EventFullName = m.EventFullName
+        ContractInfo = ""
+        LockTime =  ""
+        RefundLockTime = ""
+        EventFullName = m.EventFullName.ToString()
         LocalName = ""
+        FeeRate = ""
     }
+    
+    member this.Validate(): ErrorState =
+        {
+            ContractInfoErr =
+                validateContractInfo this.ContractInfo
+            LocalNameErr =
+                if (this.LocalName |> String.IsNullOrEmpty) then Some("You must specify LocalName") else None
+            LockTimeErr =
+                validateLockime this.LockTime
+            RefundLockTimeErr =
+                validateLockime this.RefundLockTime
+            EventFullNameErr =
+                validateEventFullName this.EventFullName
+            FeeRateErr =
+                validateFeeRate this.FeeRate
+        }
+        
+    member this.ToDomainModel() =
+        result {
+            let! c =  tryParseContractInfo this.ContractInfo
+            let! localName = if (this.LocalName |> String.IsNullOrEmpty) then Error("You must specify LocalName") else Ok(this.LocalName)
+            let! lockTime = tryParseLockTime this.LockTime
+            let! rLockTime = tryParseLockTime this.RefundLockTime
+            let! f = tryParseFeeRate this.FeeRate
+            let! e = tryParseEventFullname this.FeeRate
+            return {
+                OfferDomainModel.ContractInfo = c
+                LockTime = lockTime
+                RefundLockTime = rLockTime
+                EventFullName = e
+                LocalName = localName
+                FeeRate = f
+            }
+        }
+        
+    member this.HasError: bool =
+        let v = this.Validate()
+        match v.ContractInfoErr, v.LocalNameErr, v.LockTimeErr, v.RefundLockTimeErr, v.EventFullNameErr with
+        | None, None, None, None, None -> false
+        | _ -> true
+        
     member this.ToOfferMsg() =
         task {
             let o = Offer()
             if (this.ContractInfo.Count() < 2) then return Error("You must specify at least 2 contract info!") else
             if (this.LocalName |> String.IsNullOrEmpty) then  return Error("You must specify LocalName") else
-            o.ContractInfo <- this.ContractInfo |> Array.ofSeq
-            o.Timeouts <-
-                let t = Timeouts()
-                t.ContractMaturity <- (this.LockTime)
-                t.ContractTimeout <- (this.RefundLockTime)
-                t
             return Ok o
         }
 type State =
@@ -61,9 +167,16 @@ type State =
         Error: string option
     }
     
+type OfferUpdate =
+    | ContractInfoUpdate of string
+    | LockTimeUpdate of string
+    | RefundLockTimeUpdate of string
+    | LocalNameUpdate of string
+    | FeeRateUpdate of string
+    | EventFullnameUpdate of string
 type InternalMsg =
     | NewOffer of NewOfferMetadata
-    | UpdateOffer of OfferVM
+    | UpdateOffer of OfferUpdate
     | CommitOffer
     | InvalidInput of msg: string
     
@@ -93,16 +206,51 @@ let init: State * Cmd<InternalMsg> =
       }, Cmd.none
     
     
-let tryGetDLC globalConfig (dlcName: string) = task {
+[<AutoOpen>]
+module private Tasks =
+    let tryGetDLC globalConfig (dlcName: string) = task {
         return failwith ""
     }
+    
+    let getEvent(): Task<Repository.Event> = task {
+        return failwith ""
+    }
+
+    let tryCreateDLC globalConfig (o: OfferVM) = task {
+        let d = o.ToDomainModel() |> function Error e -> failwithf "Unreachable state: %A. Error %s" o e | Ok r -> r
+        let! maybeExistingDLC = tryGetDLC globalConfig o.LocalName
+        match maybeExistingDLC with
+        | Some _ ->
+            return Error("DLC with the same name already exists!")
+        | _ ->
+        let builder = DLCTransactionBuilder(true, null, null, null, globalConfig.Network)
+        
+        let! evt = getEvent()
+        let! dlc = (ConfigUtils.repository globalConfig).NewDLC(evt.EventId, builder)
+        let nameRepo = ConfigUtils.nameRepo globalConfig
+        do! nameRepo.AsDLCNameRepository().SetMapping(o.LocalName, dlc.Id)
+        return Ok()
+    }
+
 let update (globalConfig) (msg: InternalMsg) (state: State) =
     match msg with
     | NewOffer data ->
         let o = OfferVM.FromMetadata(data)
         {state with OfferInEdit = (o)}, Cmd.none
-    | UpdateOffer vm ->
-        { state with OfferInEdit = (vm); Error = None }, Cmd.none
+    | UpdateOffer update ->
+        match update with
+        | ContractInfoUpdate u ->
+            { state with OfferInEdit = { state.OfferInEdit with ContractInfo = u }; }, Cmd.none
+        | LockTimeUpdate u ->
+            { state with OfferInEdit = { state.OfferInEdit with LockTime = u }; }, Cmd.none
+        | RefundLockTimeUpdate u ->
+            { state with OfferInEdit = { state.OfferInEdit with RefundLockTime = u }; }, Cmd.none
+        | LocalNameUpdate u ->
+            { state with OfferInEdit = { state.OfferInEdit with LocalName = u }; }, Cmd.none
+        | FeeRateUpdate u ->
+            { state with OfferInEdit = { state.OfferInEdit with FeeRate = u }; }, Cmd.none
+        | EventFullnameUpdate u ->
+            { state with OfferInEdit = { state.OfferInEdit with EventFullName = u }; }, Cmd.none
     | InvalidInput msg ->
         { state with Error = Some (msg)}, Cmd.none
     | CommitOffer ->
@@ -126,14 +274,10 @@ let view (state: State) (dispatch) =
                 TextBox.useFloatingWatermark true
                 TextBox.name "EventFullname"
                 TextBox.watermark "Event full name: (<oracle/event>)"
-                TextBox.text (state.OfferInEdit.EventFullName.ToString())
-                TextBox.errors (state.Error |> Option.toArray |> Seq.cast<obj>)
+                TextBox.text (state.OfferInEdit.EventFullName)
+                TextBox.errors (state.OfferInEdit.Validate().EventFullNameErr |> Option.toArray |> Seq.cast<obj>)
                 yield! TextBox.onTextInput(fun s ->
-                    match EventFullName.TryParse s with
-                    | true, eventFullName ->
-                        let newOffer = { state.OfferInEdit with EventFullName = eventFullName }
-                        newOffer |> UpdateOffer |> ForSelf |> dispatch
-                    | _ -> "Invalid EventFullName! (It must be in the form of 'oraclename/eventname'" |> InvalidInput |> ForSelf |> dispatch
+                    s |> EventFullnameUpdate |>  UpdateOffer |> ForSelf |> dispatch
                     )
             ]
             TextBox.create [
@@ -141,30 +285,51 @@ let view (state: State) (dispatch) =
                 TextBox.useFloatingWatermark true
                 TextBox.name "Payoff"
                 TextBox.watermark "Payoffs. a.k.a contract_info: (outcome:reward,outcome:-loss)"
+                TextBox.text (state.OfferInEdit.ContractInfo)
+                TextBox.errors (state.OfferInEdit.Validate().ContractInfoErr |> Option.toArray |> Seq.cast<obj>)
+                yield! TextBox.onTextInput(fun s ->
+                    s |> ContractInfoUpdate |>  UpdateOffer |> ForSelf |> dispatch
+                    )
             ]
             TextBox.create [
                 TextBox.classes ["userinput"]
                 TextBox.useFloatingWatermark true
                 TextBox.name "Localname"
                 TextBox.watermark "Local Name For the offer"
+                TextBox.errors (state.OfferInEdit.Validate().LocalNameErr |> Option.toArray |> Seq.cast<obj>)
+                yield! TextBox.onTextInput(fun s ->
+                    s |> LocalNameUpdate |>  UpdateOffer |> ForSelf |> dispatch
+                    )
             ]
             TextBox.create [
                 TextBox.classes ["userinput"]
                 TextBox.useFloatingWatermark true
                 TextBox.name "Locktime"
                 TextBox.watermark "CET locktime. a.k.a. 'contract_maturity_bound'"
+                TextBox.errors (state.OfferInEdit.Validate().LockTimeErr |> Option.toArray |> Seq.cast<obj>)
+                yield! TextBox.onTextInput(fun s ->
+                    s |> LockTimeUpdate |>  UpdateOffer |> ForSelf |> dispatch
+                    )
             ]
             TextBox.create [
                 TextBox.classes ["userinput"]
                 TextBox.useFloatingWatermark true
                 TextBox.name "RefundLocktime. a.k.a 'contract_timeout'"
                 TextBox.watermark "Refund locktime"
+                TextBox.errors (state.OfferInEdit.Validate().RefundLockTimeErr |> Option.toArray |> Seq.cast<obj>)
+                yield! TextBox.onTextInput(fun s ->
+                    s |> RefundLockTimeUpdate |>  UpdateOffer |> ForSelf |> dispatch
+                    )
             ]
             TextBox.create [
                 TextBox.classes ["userinput"]
                 TextBox.useFloatingWatermark true
                 TextBox.name "FeeRate"
                 TextBox.watermark "Feerate"
+                TextBox.errors (state.OfferInEdit.Validate().FeeRateErr |> Option.toArray |> Seq.cast<obj>)
+                yield! TextBox.onTextInput(fun s ->
+                    s |> FeeRateUpdate |>  UpdateOffer |> ForSelf |> dispatch
+                    )
             ]
             StackPanel.create [
                 StackPanel.dock Dock.Bottom
@@ -176,7 +341,7 @@ let view (state: State) (dispatch) =
                         Button.content "Cancel"
                     ]
                     Button.create [
-                        Button.isEnabled state.Error.IsNone
+                        Button.isEnabled (state.OfferInEdit.HasError |> not)
                         Button.classes ["round"]
                         Button.dock Dock.Right
                         Button.content "Ok"
