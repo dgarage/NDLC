ee8554e8616ef56079177d67deb1b7c7499f39b8
joe.miyamoto <joe.miyamoto@bitcoinbank.co.jp>
2020-10-07 11:13:12 +0900

    move some primitives from NDLC.CLI to NDLC

--- NDLC/Constants.cs
+++ NDLC/Constants.cs
@@ -1,6 +1,8 @@
-﻿namespace NDLC
+﻿using NBitcoin;
+
+namespace NDLC
 {
-	class Constants
+	public class Constants
 	{
 		public static readonly LockTime NeverLockTime = new LockTime(500000000 - 1);
 	}
