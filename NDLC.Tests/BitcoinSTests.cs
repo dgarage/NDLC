0105b6d659e4e1554f62173682b2a9ea0431b102
nicolas.dorier <nicolas.dorier@gmail.com>
2020-11-19 14:48:36 +0900

    Remove obsolete tests

--- NDLC.Tests/BitcoinSTests.cs
+++ NDLC.Tests/BitcoinSTests.cs
@@ -58,35 +58,6 @@ namespace NDLC.Tests
 			}
 		}
 
-		[Fact]
-		public void CanValidateCETSigs()
-		{
-			var offer = Parse<Messages.Offer>("Data/Offer2.json");
-			var accept = Parse<Messages.Accept>("Data/Accept2.json");
-			var sign = Parse<Messages.Sign>("Data/Sign2.json");
-
-			var funding = Transaction.Parse("02000000000102118df2c93e3be1f05af4fe16ba82eab135ba624bb96db5b218222ad7f99f1c400000000000ffffffff9ff82cc6f2509b2bc9f0d56fa37bea855d40b74cd2633e75ca343469508e892d0000000000ffffffff0372dc010000000000220020a9dbe7e77d7f966d4b8ab36d576e19f4c392caf039ea5e53a0078d771a3e92eefb2a042a01000000160014ec8d64ed4742016d12deac1106577f94747f09f41b5afa2f01000000160014c22a6d286c108ad458ba60e00576859a78ae8de002473044022071e5e088ba8e4434ab044350448479561ad8179bf2a88789a9459215bcf610c80220128535940b083dca52791f373b6474dd24f3a9096c039411943ffcff6fb293a801210333dea605a7d2c223de68c1189bd5e171cb94bd35712122eff08a9cdbed5dca8d02473044022059d6a3ae8cee036998065d2139459a029c4d925f952a860434910b12f9e9a3a002205871d75c28b8486caa90a6d44c3ae59eeb98bbbc90990f5b510466178eb80d9c01210310f8a8f195fe3167af0db8c6bb208e9a8ac075c99276a0150c1d374d0d13d26a00000000", Network.Main);
-			var cet = Transaction.Parse("020000000001016b3ea350dfa327d9c4c030fc3e2e49c04aae28d6020f1624202c57bf620dddbc0000000000feffffff01a08601000000000016001404bea358f0a1c3bdabe6148fd2ea44838110ef830400483045022100c3d1901ae606851946edaaa007cc5ed3ba05a1a812e76d8e4f5a3b034fe11dd4022063d51c44bc8a78ee752e735e3c84be2329e0eb15022db552a1181eaf1edb7dff0147304402205f5ba5027909abef13c61bc604eb2deaf223549213c6c7c9456d972ae1badf800220348bf4918b01bea8ee69d104a60254ad23159654a8f8b31e43da1fc53603c2390147522102d63fec4b8a6705a34b32e5476a5a07c20774fe2abcf88eeea320c9714bc3010e21038b22637639db00f3dd5d8c46644b61954a01362bc92a87eb136bd6ba80c1f12852ae64000000", Network.Main);
-			RemoveSigs(cet);
-			RemoveSigs(funding);
-
-
-			foreach (var isInitiator in new[] { true, false })
-			{
-				var b = new DLCTransactionBuilder(isInitiator, offer, accept, sign, funding, Network.RegTest);
-				var actualCet = b.BuildCET(offer.ContractInfo[1].Outcome);
-				Assert.Equal(cet.ToString(), actualCet.ToString());
-				Assert.True(b.VerifyRemoteCetSigs(isInitiator ? accept.CetSigs.OutcomeSigs : sign.CetSigs.OutcomeSigs));
-				Assert.True(b.VerifyRemoteRefundSignature());
-
-				b = new DLCTransactionBuilder(isInitiator, offer, accept, sign, Network.RegTest);
-				var actualFunding = b.GetFundingTransaction();
-				RemoveSigs(actualFunding);
-				Assert.Equal(funding.ToString(), actualFunding.ToString());
-			}
-
-		}
-
 		private static void RemoveSigs(Transaction cet)
 		{
 			foreach (var input in cet.Inputs)
@@ -108,43 +79,6 @@ namespace NDLC.Tests
 				Assert.Equal(sigPoint.Q, Context.Instance.CreateECPrivKey(sig.s).CreatePubKey().Q);
 			}
 		}
-
-		[Fact]
-		public void FullExchange()
-		{
-			var offerExample = Parse<Messages.Offer>("Data/Offer2.json");
-			var offerKey = new Key();
-			var acceptKey = new Key();
-			var initiatorInputKey = new Key();
-			var acceptorInputKey = new Key();
-			var initiator = new DLCTransactionBuilder(true, null, null, null, Network.RegTest);
-			var requiredFund = initiator.Offer(offerExample.OracleInfo.PubKey,
-								  offerExample.OracleInfo.RValue,
-								  DiscretePayoffs.CreateFromContractInfo(offerExample.ContractInfo, offerExample.TotalCollateral,
-								  new[] { new DiscreteOutcome("Republicans_win"), new DiscreteOutcome("Democrats_win"), new DiscreteOutcome("other") }),
-								  offerExample.Timeouts);
-			var fund1 = GetFundingPSBT(initiatorInputKey, requiredFund);
-			var offer = initiator.FundOffer(offerKey, fund1);
-			var acceptor = new DLCTransactionBuilder(false, null, null, null, Network.RegTest);
-			var acceptorPayoff = acceptor.Accept(offer);
-			var fund2 = GetFundingPSBT(acceptorInputKey, acceptorPayoff.CalculateMinimumCollateral());
-			var accept = acceptor.FundAccept(acceptKey, fund2);
-			initiator.Sign1(accept);
-			var fundPSBT = initiator.GetFundingPSBT();
-			fundPSBT.SignWithKeys(initiatorInputKey);
-			var sign = initiator.Sign2(offerKey, fundPSBT);
-
-			acceptor.Finalize1(sign);
-			fundPSBT = acceptor.GetFundingPSBT();
-			fundPSBT.SignWithKeys(acceptorInputKey);
-			var fullyVerified = acceptor.Finalize(fundPSBT);
-			foreach (var i in fullyVerified.Inputs)
-				Assert.NotNull(i.WitScript);
-			fundPSBT = acceptor.GetFundingPSBT();
-			if (fundPSBT.TryGetEstimatedFeeRate(out var estimated))
-				Assert.True(estimated > new FeeRate(1.0m), "Fee Rate of the funding PSBT are too low");
-		}
-
 		[Fact]
 		public void CanConvertContractInfoToPayoff()
 		{
@@ -176,140 +110,6 @@ namespace NDLC.Tests
 				//Assert.Equal(sig.rx, nonce.fe);
 				Assert.True(privKey.CreateXOnlyPubKey().SigVerifyBIP340(sig, msg));
 			}
-		}
-		[Fact]
-		public void FullExchange2()
-		{
-			var initiatorInputKey = new Key();
-			var acceptorInputKey = new Key();
-
-			var offerKey = new Key();
-			var acceptKey = new Key();
-
-			var initiator = new DLCTransactionBuilder(true, null, null, null, Network.RegTest);
-			var acceptor = new DLCTransactionBuilder(false, null, null, null, Network.RegTest);
-
-			var oracleInfo = OracleInfo.Parse("e4d36e995ff4bba4da2b60ad907d61d36e120d6f7314a3c2a20c6e27a5cd850ff67f8f41718c86f05eb95fab308f5ed788a2a963124299154648f97124caa579");
-			var requiredCollateral = initiator.Offer(oracleInfo.PubKey, oracleInfo.RValue,
-				new DiscretePayoffs() {
-				new DiscretePayoff("Republicans", Money.Coins(0.4m)),
-				new DiscretePayoff("Democrats", -Money.Coins(0.6m)),
-				new DiscretePayoff("Smith", Money.Zero) }, new Timeouts()
-				{
-					ContractMaturity = 100,
-					ContractTimeout = 200
-				});
-			var fund1 = GetFundingPSBT(acceptorInputKey, requiredCollateral);
-			var offer = initiator.FundOffer(offerKey, fund1);
-
-			var payoff = acceptor.Accept(offer);
-			var fund2 = GetFundingPSBT(initiatorInputKey, payoff.CalculateMinimumCollateral());
-			var accept = acceptor.FundAccept(acceptKey, fund2);
-			initiator.Sign1(accept);
-			var fundPSBT = initiator.GetFundingPSBT();
-			fundPSBT.SignWithKeys(initiatorInputKey);
-			var sign = initiator.Sign2(offerKey, fundPSBT);
-
-			acceptor.Finalize1(sign);
-			fundPSBT = acceptor.GetFundingPSBT();
-			fundPSBT.SignWithKeys(acceptorInputKey);
-			var fullyVerified = acceptor.Finalize(fundPSBT);
-			foreach (var i in fullyVerified.Inputs)
-				Assert.NotNull(i.WitScript);
-
-			var cet = initiator.BuildCET(offer.ContractInfo[0].Outcome);
-			var keyBytes = Encoders.Hex.DecodeData("d1da46f96f0be50bce2bbabe6bc8633f448ec3f1d14715a0b086b68ed34e095d");
-			var oracleSecret = new Key(keyBytes);
-
-			var sig = oracleInfo.RValue.CreateSchnorrSignature(oracleSecret.ToECPrivKey());
-			Assert.True(oracleInfo.PubKey.SigVerifyBIP340(sig, new DiscreteOutcome("Republicans").Hash));
-
-			var execution = initiator.Execute(offerKey, oracleSecret);
-			// Can extract?
-			initiator.ExtractAttestation(execution.CET);
-			acceptor.ExtractAttestation(execution.CET);
-
-			this.testOutputHelper.WriteLine("----Final state------");
-			testOutputHelper.WriteLine(JObject.Parse(initiator.ExportState()).ToString(Formatting.Indented));
-			this.testOutputHelper.WriteLine("---------------------");
-		}
-
-		[Fact]
-		public void CanCalculateEventId()
-		{
-			var offer = Parse<Messages.Offer>("Data/Offer2.json");
-			var accept = Parse<Messages.Accept>("Data/Accept2.json");
-
-			var builder = new DLCTransactionBuilder(false, null, null, null, Network.RegTest);
-			var payoff = builder.Accept(offer);
-			PSBT fundPSBT = GetFundingPSBT(new Key(), payoff.CalculateMinimumCollateral());
-			var accept2 = builder.FundAccept(new Key(), fundPSBT);
-			Assert.Equal(accept.EventId, accept2.EventId);
-		}
-
-		[Fact]
-		public void CanCreateAccept()
-		{
-			var offer = Parse<Messages.Offer>("Data/Offer2.json");
-			offer.SetContractPreimages(
-				new DiscreteOutcome("Republicans_win"),
-				new DiscreteOutcome("Democrats_win"),
-				new DiscreteOutcome("other")
-				);
-			var builder = new DLCTransactionBuilder(false, null, null, null, Network.RegTest);
-			var fundingInputKey = new Key();
-			var payoffs = builder.Accept(offer);
-			PSBT fundPSBT = GetFundingPSBT(fundingInputKey, payoffs.CalculateMinimumCollateral());
-			var accept = builder.FundAccept(new Key(), fundPSBT);
-
-			builder = new DLCTransactionBuilder(true, offer, null, null, Network.RegTest);
-			builder.Sign1(accept);
-		}
-		private static PSBT GetFundingPSBT(Key ownedCoinKey, Money collateral)
-		{
-			var c1 = new Coin(new OutPoint(RandomUtils.GetUInt256(), 0), new TxOut(Money.Coins(2.0m), ownedCoinKey.PubKey.GetScriptPubKey(ScriptPubKeyType.Segwit)));
-			var txbuilder = Network.RegTest.CreateTransactionBuilder();
-			txbuilder.AddCoins(c1);
-			txbuilder.Send(new Key().ScriptPubKey, collateral);
-			txbuilder.SendEstimatedFees(new FeeRate(1.0m));
-			txbuilder.SetChange(new Key().ScriptPubKey);
-			var fundPSBT = txbuilder.BuildPSBT(false);
-			return fundPSBT;
-		}
-
-		[Fact]
-		public void CanCheckMessages()
-		{
-			var offer = Parse<Messages.Offer>("Data/Offer.json");
-			Assert.Equal("SHA256:cbaede9e2ad17109b71b85a23306b6d4b93e78e8e8e8d830d836974f16128ae8", offer.ContractInfo[1].Outcome.ToString());
-			Assert.Equal(200000000L, offer.ContractInfo[1].Payout.Satoshi);
-			Assert.Equal(100000000L, offer.TotalCollateral.Satoshi);
-
-			var accept = Parse<Messages.Accept>("Data/Accept.json");
-			Assert.Equal(100000000L, accept.TotalCollateral.Satoshi);
-			Assert.Equal(2, accept.CetSigs.OutcomeSigs.Length);
-			Assert.Equal("00595165a73cc04eaab13077abbffae5edf0c371b9621fad9ea28da00026373a853bcc3ac24939d0d004e39b96469b2173aa20e429ca3bffd3ab0db7735ad6d87a012186ff2afb8c05bca05ad8acf22aecadf47f967bb81753c13c3b081fc643c8db855283e554359d1a1a870d2b016a9db6e6838f5ca1afb1508aa0c50fd9d05ac60a7b7cc2570b62426d467183baf109fb23a5fdf37f273c087c23744c6529f353", accept.CetSigs.OutcomeSigs[0].ToString());
-			var str = JsonConvert.SerializeObject(accept, Settings);
-			accept = JsonConvert.DeserializeObject<Accept>(str, Settings);
-			Assert.Equal("00595165a73cc04eaab13077abbffae5edf0c371b9621fad9ea28da00026373a853bcc3ac24939d0d004e39b96469b2173aa20e429ca3bffd3ab0db7735ad6d87a012186ff2afb8c05bca05ad8acf22aecadf47f967bb81753c13c3b081fc643c8db855283e554359d1a1a870d2b016a9db6e6838f5ca1afb1508aa0c50fd9d05ac60a7b7cc2570b62426d467183baf109fb23a5fdf37f273c087c23744c6529f353", accept.CetSigs.OutcomeSigs[0].ToString());
-
-			var sign = Parse<Messages.Sign>("Data/Sign.json");
-			var sig = sign.FundingSigs[0][0].ToString(); ;
-			Assert.Equal("220202f37b2ca55f880f9d73b311a4369f2f02fbadefc037628d6eaef98ec222b8bcb046304302206589a41139774c27c242af730ae225483ba264eeec026952c6a6cd0bc8a7413c021f2289c32cfb7b4baa5873e350675133de93cfc69de50220fddafbc6f23a46e201", sig);
-
-			CanRoundTrip<Messages.Accept>("Data/Accept.json");
-			CanRoundTrip<Messages.Offer>("Data/Offer.json");
-			CanRoundTrip<Messages.Sign>("Data/Sign.json");
-		}
-
-		private void CanRoundTrip<T>(string file)
-		{
-			var content = File.ReadAllText(file);
-			var data = JsonConvert.DeserializeObject<T>(content, Settings);
-			var back = JsonConvert.SerializeObject(data, Settings);
-			var expected = JObject.Parse(content);
-			var actual = JObject.Parse(back);
-			Assert.Equal(expected.ToString(Formatting.Indented), actual.ToString(Formatting.Indented));
 		}
 
 		private static T Parse<T>(string file, JsonSerializerSettings settings = null)
@@ -405,51 +205,6 @@ namespace NDLC.Tests
 		//		testOutputHelper.WriteLine($"{form}: {Encoders.Hex.EncodeData(Encoding.UTF8.GetBytes(elephant.Normalize(form)))}");
 		//	}
 		//}
-
-		[Fact]
-		public void AcceptorTestVectors()
-		{
-			RunAcceptorTest("Data/Acceptor-Chris", Network.TestNet);
-			RunAcceptorTest("Data/Acceptor-Chris2", Network.TestNet);
-		}
-		void RunAcceptorTest(string acceptorFolder, Network network)
-		{
-			var myKey = new Key(Encoders.Hex.DecodeData("659ed592d16a47f8b3eea2e3d918624963e20da29a83e097c0ffbf0ef1b3e8cc"));
-			var data = AcceptorTest.Open(acceptorFolder, network);
-
-			if (data.FundingOverride != null)
-			{
-				testOutputHelper.WriteLine("Using funding override");
-				data.Builder.FundingOverride = data.FundingOverride;
-			}
-
-			data.Builder.Accept(data.Offer, data.FundingTemplate.Outputs[0].Value);
-			var accepted = data.Builder.FundAccept(myKey, data.FundingTemplate);
-			testOutputHelper.WriteLine("---Accept message---");
-			testOutputHelper.WriteLine(JsonConvert.SerializeObject(accepted, TestnetSettings));
-			testOutputHelper.WriteLine("--------------------");
-			if (data.FundingOverride == null)
-			{
-				testOutputHelper.WriteLine("----Expected funding ----");
-				testOutputHelper.WriteLine(data.Builder.GetFundingTransaction().ToHex());
-				testOutputHelper.WriteLine("--------------------");
-			}
-
-			data.Builder.Finalize1(data.Sign);
-			var unsigned = data.Builder.GetFundingPSBT();
-			testOutputHelper.WriteLine("---Partially signed funding PSBT---");
-			testOutputHelper.WriteLine(unsigned.ToBase64());
-			testOutputHelper.WriteLine("--------------------");
-
-			if (data.OracleAttestation != null)
-			{
-				var outcomeSigned = data.Builder.Execute(myKey, data.OracleAttestation);
-
-				testOutputHelper.WriteLine("---Signed CET---");
-				testOutputHelper.WriteLine(outcomeSigned.CET.ToHex());
-				testOutputHelper.WriteLine("--------------------");
-			}
-		}
 
 		[Fact]
 		public void testAdaptorVerify()
