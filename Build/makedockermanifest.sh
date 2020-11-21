4f322c9318424fcd10a29b2f9f9e098a356c7bc2
nicolas.dorier <nicolas.dorier@gmail.com>
2020-11-21 11:18:20 +0900

    Push latest image as well

--- Build/makedockermanifest.sh
+++ Build/makedockermanifest.sh
@@ -3,6 +3,7 @@
 source ./Build/utils.sh
 
 DOCKER_TAG="$DOCKERHUB_USER/$DOCKERHUB_REPO:$VERSION"
+DOCKER_TAG_LATEST="$DOCKERHUB_USER/$DOCKERHUB_REPO"
 
 echo "Docker tag: $DOCKER_TAG"
 
@@ -12,8 +13,10 @@ sudo sh -c 'echo "{ \"experimental\": \"enabled\" }" > $HOME/.docker/config.json
 sudo docker login "--username=$DOCKERHUB_USER" "--password=$DOCKER_API_KEY"
 #
 
-sudo docker manifest create --amend $DOCKER_TAG $DOCKER_TAG-amd64 $DOCKER_TAG-arm32v7 $DOCKER_TAG-arm64v8
-sudo docker manifest annotate $DOCKER_TAG $DOCKER_TAG-amd64 --os linux --arch amd64
-sudo docker manifest annotate $DOCKER_TAG $DOCKER_TAG-arm32v7 --os linux --arch arm --variant v7
-sudo docker manifest annotate $DOCKER_TAG $DOCKER_TAG-arm64v8 --os linux --arch arm64 --variant v8
-sudo docker manifest push $DOCKER_TAG -p
+for target in "$DOCKER_TAG" "$DOCKER_TAG_LATEST"; do
+	sudo docker manifest create --amend $target $DOCKER_TAG-amd64 $DOCKER_TAG-arm32v7 $DOCKER_TAG-arm64v8
+	sudo docker manifest annotate $target $DOCKER_TAG-amd64 --os linux --arch amd64
+	sudo docker manifest annotate $target $DOCKER_TAG-arm32v7 --os linux --arch arm --variant v7
+	sudo docker manifest annotate $target $DOCKER_TAG-arm64v8 --os linux --arch arm64 --variant v8
+	sudo docker manifest push $target -p
+done
